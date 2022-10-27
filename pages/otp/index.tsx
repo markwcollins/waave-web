4ecec59e7d3adc039d9f525a95424e4bd35defc9
“Mark <mark.william.collins@gmail.com>
2022-10-28 09:24:34 +1100

    updates

--- pages/otp/index.tsx
+++ pages/otp/index.tsx
@@ -9,10 +9,18 @@ export default function Page() {
   )
 }
 
+interface IToken {
+  token: string
+}
+
+const initialValues: IToken = {
+  token: ''
+}
+
 
 const AutoSubmitToken = () => {
   // Grab values and submitForm from context
-  const { values, submitForm } = useFormikContext();
+  const { values, submitForm } = useFormikContext<IToken>();
   React.useEffect(() => {
     // Submit the form imperatively as an effect as soon as form values.token are 6 digits long
     if (values.token.length === 6) {
@@ -27,9 +35,9 @@ const TwoFactorVerificationForm = () => (
     <h1>2-step Verification</h1>
     <p>Please enter the 6 digit code sent to your device</p>
     <Formik
-      initialValues={{ token: '' }}
+      initialValues={initialValues}
       validate={values => {
-        const errors = {};
+        const errors = {} as any;
         if (values.token.length < 5) {
           errors.token = 'Invalid code. Too short.';
         }
